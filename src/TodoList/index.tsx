e1f64bdd0c27783817c35c0ec6d6813b5abe3972
Muhammad Zaurbekov <fewyearsago@mail.ru>
2023-06-19 16:58:00 +0300

    fix

--- src/TodoList/index.tsx
+++ src/TodoList/index.tsx
@@ -4,20 +4,18 @@ import style from './index.module.scss';
 interface item {
   id: number;
   text: string;
-  completed: boolean;
 }
 const TodoList: React.FC = () => {
   const [todos, setTodos] = React.useState<item[]>([
     {
       id: 1,
       text: 'Hello',
-      completed: false,
     },
   ]);
   const [input, setInput] = React.useState<string>('');
   const handleClick = () => {
     if (input === '') return;
-    const newTodo: item = { id: Date.now(), text: input, completed: false };
+    const newTodo: item = { id: Date.now(), text: input };
     setTodos([...todos, newTodo]);
     setInput('');
   };
@@ -27,20 +25,24 @@ const TodoList: React.FC = () => {
   return (
     <div className={style.root}>
       <h1>To-Do List</h1>
-      <ul>
-        {todos.map((e, i) => (
-          <li key={i}>
-            {e.text}
-            <button onClick={() => handleToogle(e.id)}>-</button>
-          </li>
-        ))}
-      </ul>
+      {todos.length === 0 ? (
+        <p>Список пуст</p>
+      ) : (
+        <ul>
+          {todos.map((e, i) => (
+            <li key={i}>
+              {e.text}
+              <button onClick={() => handleToogle(e.id)}>-</button>
+            </li>
+          ))}
+        </ul>
+      )}
       <div>
         <input
           value={input}
           onChange={(e) => setInput(e.currentTarget.value)}
           type="text"
-          placeholder="add item"
+          placeholder="Введите заметку"
         />
         <button onClick={handleClick}>+</button>
       </div>
